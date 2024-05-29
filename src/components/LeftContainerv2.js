bc4bec83f5e74a27f6aafc8d061c2b6838157889
dummymind <riboncyber@gmail.com>
2024-05-29 17:29:30 +0530

    cards with first stage

--- src/components/LeftContainerv2.js
+++ src/components/LeftContainerv2.js
@@ -50,7 +50,67 @@ function LeftContainer() {
 
 
 
-            
+            <div className="card" style={{ width: "18rem" }}>
+  <img className="card-img-top" src="..." alt="Card image cap" />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </p>
+    <a href="#" className="btn btn-primary">
+      Go somewhere
+    </a>
+  </div>
+</div>
+
+
+            <div className="card-group">
+  <div className="card rounded m-2">
+    <img className="card-img-top-custom card-img-top" src={rectangelimage}   alt="Card image cap" />
+    <div className="card-body">
+      <h5>
+                    Lunch&amp;Learn at Royal Canin’s St Charles site
+                    </h5>
+      <p className="card-text">
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </p>
+    </div>
+    <div className="card-footer">
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div className="card rounded m-2">
+    <img className="card-img-top-custom card-img-top" src={rectangelimage2} alt="Card image cap" />
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        This card has supporting text below as a natural lead-in to additional
+        content.
+      </p>
+    </div>
+    <div className="card-footer">
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div className="card rounded m-2">
+    <img className="card-img-top-custom card-img-top" src={rectangelimage} alt="Card image cap" />
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </p>
+    </div>
+    <div className="card-footer">
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+
+
 
 
  
