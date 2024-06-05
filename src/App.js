06bc1183dc4804bf72220d93e94297b9c28103df
dummymind <riboncyber@gmail.com>
2024-06-05 11:56:38 +0530

    vip v2

--- src/App.js
+++ src/App.js
@@ -22,6 +22,19 @@ function App() {
 
         <Vip />
 
+        <div className="container-fluid">
+        <div className="progress d-flex flex-column" style={{ backgroundColor: 'gainsboro', height: '100%' }}>
+          <div className="row">
+            <div className="col-md-7">
+              <LeftContainer />
+            </div>
+            <div className="col-md-4">
+              <RightContainer />
+            </div>
+          </div>
+        </div>
+      </div>
+
 
       </div>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
